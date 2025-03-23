6588c96f0e20851bdc1896ec1d54bdd5abae9b97
Atamanyuk2008 <atamaniuk.aleksandr@gmail.com>
2025-03-23 20:08:53 +0200

    Add files via upload

---
+++ 26n3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26n3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = new int[] { 1, 2, 3, 4, 5 };
+            Input(mass);
+            Console.Read();
+
+        }
+        static void Input(int[] mass)
+        {
+            foreach (int i in mass)
+            {
+                Console.WriteLine(mass[0]);
+                Console.WriteLine(mass[1]);
+                Console.WriteLine(mass[2]);
+                Console.WriteLine(mass[3]);
+                Console.WriteLine(mass[4]);
+                Console.Read();
+
+            }
+        }
+    }
+
+    }
+
