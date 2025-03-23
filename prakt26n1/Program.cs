74e595cc2b4e06acb7997f2156adce9e0b0b1e3b
Atamanyuk2008 <atamaniuk.aleksandr@gmail.com>
2025-03-23 20:04:59 +0200

    Add files via upload

---
+++ prakt26n1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt26n1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] mass =new int[Size()];
+           Massiv(mass);
+           Output(mass);
+           Console.ReadKey();
+       }
+       static int Size()
+       {
+           Console.WriteLine("n= ");
+           int n =int.Parse(Console.ReadLine());
+           return n;
+       }
+       static void Massiv(int[] mass)
+       {
+
+           for (int i = 0; i < mass.Length; i++)
+           {
+               Console.WriteLine($"mass[{i}]");
+               mass[i]= int.Parse(Console.ReadLine());
+           }    
+
+       }
+       static void Output(int[] mass)
+       {
+           for(int i = 0; i <mass.Length ;i++)
+           {
+               Console.WriteLine($"massiv[{i}]={mass[(i)]}");
+           }
+        }
+    }
+}
